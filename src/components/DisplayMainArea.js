433b9a2328d9341dca2e9b6c0ca7de82950b3bed
Zalewski <ezalewski@blackbirdlabs.com>
2020-09-15 10:14:42 -0400

    Added menu

--- src/components/DisplayMainArea.js
+++ src/components/DisplayMainArea.js
@@ -11,12 +11,55 @@ class DisplayMainArea extends React.Component {
       <>
       <div className="displayArea">
         <div className="menuSummary">
-          <p>This area has the menu.</p>
-          <p>Or the order summary.</p>
+            <h2>Menu</h2>
+            <div className="leftPanel">
+            <h3>Sizes</h3>
+            <hr/>
+            <p>Small 10"</p>
+            <p>Medium 12"</p>
+            <p>Large 14"</p>
+            <p>X-Large 16"</p>
+            <h3>Crust</h3>
+            <hr/>
+            <p>Original</p>
+            <p>Thin Crust</p>
+            <p>Gluten Free</p>
+            <h3>Cheese</h3>
+            <hr/>
+            <p>None</p>
+            <p>Light</p>
+            <p>Normal</p>
+            <p>Double</p>
+            <h3>Sauce</h3>
+            <hr/>
+            <p>Tomato</p>
+            <p>Marinera</p>
+            <p>Honey BBQ</p>
+            <p>Alfredo</p>
+            </div>
+
+            <div className="rightPanel">
+            <h3>Meats</h3>
+            <hr/>
+            <p>Pepeproni</p>
+            <p>Beef</p>
+            <p>Philly Steak</p>
+            <p>Italian Sausage</p>
+            <p>Grilled Chicken</p>
+            <p>Bacon</p>
+            <h3>Toppings</h3>
+            <hr/>
+            <p>Tomatoes</p>
+            <p>Mushrooms</p>
+            <p>Jalapeño Peppers</p>
+            <p>Onions</p>
+            <p>Pineapple</p>
+            <p>Green Peppers</p>
+            </div>
         </div>
       </div>
-      <div className="container">        
-        <SimpleForm />
+      <div className="chatBot">
+          <SimpleForm />
       </div>
       </>
     );
