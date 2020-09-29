7fb71f9c6fabee800146e9d476cd2c73292f7f30
Obeyd Khan <Obeyd.Khan@asurion.com>
2020-09-29 10:19:34 -0400

    Updates

---
+++ precache-manifest.6ade22399719bf0669370347dd7b79d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f170738bad5a203b5d157f163d69274",
+    "url": "/pizza-chatbot/index.html"
+  },
+  {
+    "revision": "d2f6ec700a194c1888b8",
+    "url": "/pizza-chatbot/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "6b039941c57b1ba25b39",
+    "url": "/pizza-chatbot/static/css/main.b01802fa.chunk.css"
+  },
+  {
+    "revision": "d2f6ec700a194c1888b8",
+    "url": "/pizza-chatbot/static/js/2.cb7f9969.chunk.js"
+  },
+  {
+    "revision": "b2d63f0f1d1b46d97af408c30a0920b0",
+    "url": "/pizza-chatbot/static/js/2.cb7f9969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b039941c57b1ba25b39",
+    "url": "/pizza-chatbot/static/js/main.076490a7.chunk.js"
+  },
+  {
+    "revision": "4c773cc615b121c9cbb4",
+    "url": "/pizza-chatbot/static/js/runtime-main.b41a950a.js"
+  }
+]);
