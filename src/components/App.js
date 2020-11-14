5e1786d3e5d1f01b26f9b1b6541a5f73d9e61575
ObeydKhan <kobeydullah@yahoo.com>
2020-11-14 17:59:32 -0500

    Weakness 3 - v2

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import '../css/App.css';
 import DisplayMainArea from './DisplayMainArea';
 import logo from '../resources/SliceLogo.png';
 import Order from './PizzaOrder';
-
 import StoreLoc from './StoreLoc';
 
 class App extends React.Component {
@@ -57,7 +56,10 @@ class App extends React.Component {
     );
     return banner;
   }
-  render(){                 
+  render(){
+     const arrowRight = <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+     <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+    </svg>;                 
     const banner = this.StoreBanner;
     return (
       <div className="mainPageArea">
@@ -65,11 +67,32 @@ class App extends React.Component {
         <a href="/pizza-chatbot"><img src={logo} alt="Logo"></img>
         <h1>Slice</h1></a>
         {banner}
-        <a href="/pizza-chatbot"><p>Search for location</p></a>
-        <a href="/locations"><p>Select location</p></a>
-        <a href="/slicebot"><p>Order pizzas</p></a>
-        <a href="/review"><p>Review order</p></a>
-        <a href="/summary"><p>Payment 	&amp; Confirmation</p></a>
+        <div className="container">
+        <div className="progress">
+          <div className="progress-bar bg-warning progress-bar-striped progress-bar-animated" role="progressbar" style={{width: '25%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <ul className="nav justify-content-center info">
+          <li className="nav-item">
+            <a className="nav-link active" href="/pizza-chatbot">Search for location</a>
+          </li>
+          {arrowRight}
+          <li className="nav-item">
+            <a className="nav-link" onClick={()=> console.log('Hello!')}>Select location</a>
+          </li>
+          {arrowRight}
+          <li className="nav-item">
+            <a className="nav-link" href="/slicebot">Order pizzas</a>
+          </li>
+          {arrowRight}
+          <li className="nav-item">
+            <a className="nav-link disabled" href="/review">Review order</a>
+          </li>
+          {arrowRight}
+          <li className="nav-item">
+            <a className="nav-link disabled" href="/summary">Payment 	&amp; Confirmation</a>
+          </li>
+        </ul>
+        </div>
       </header>      
       <StoreLoc appState={this.state} 
         set={this.setStore} update={this.updateLoc} forwardedRef={this.input}/>      
